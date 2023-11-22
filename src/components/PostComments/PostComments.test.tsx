be8b30695bb8611221e680e86d81c638c94032ec
Istan <thiago.sh26@gmail.com>
2023-11-22 20:27:36 -0300

    realizando  a atividade de teste de dois comentarios

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -2,9 +2,32 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import Post from '.';
 import PostComment from '.';
 
-describe('Teste para o componente PostComment', () => {
+describe('Teste para o componente Post', () => { 
+    
     it('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+it('Deve adicionar dois comentários', () => {
+    render(<PostComment/>);
+
+
+    
+    fireEvent.change(screen.getByTestId('comment-textarea'), {
+        target: {
+            value: 'Primeiro comentario adicionado',
+        }
+    });
+    fireEvent.click(screen.getByTestId('comment-button'));
+
+    fireEvent.change(screen.getByTestId('comment-textarea'), {
+        target: {
+            value: 'Segundo comentário adicionado',
+        }
+    });
+    fireEvent.click(screen.getByTestId('comment-button'));
+
+    expect(screen.getAllByTestId('comment-element')).toHaveLength(2);
+});
 });
